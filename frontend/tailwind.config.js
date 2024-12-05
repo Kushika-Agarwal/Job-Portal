ecf7d247e613f4849115f068fd2b9c629fa992ea
Kushika-Agarwal <112795633+Kushika-Agarwal@users.noreply.github.com>
2024-12-05 20:04:19 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,9 @@
 // }
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+
+export default{
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
